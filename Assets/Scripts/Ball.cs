93a70d4f9975b1bf7eee132176f6332aca038e9b
unkeptroadrash <unkeptroadrash@outlook.com>
2021-02-11 22:01:36 -0800

    Added the ability to launch the ball from the paddle as well as cleaned up the code a bit

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,12 @@ public class Ball : MonoBehaviour
 {
     // configuration parameters
     [SerializeField] Paddle paddle;
+    [SerializeField] float velocityX = 2f;
+    [SerializeField] float velocityY = 10f;
 
     // state
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,24 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (hasStarted == false)
+        {
+            LockBallToPaddle();
+            LaunchBallOnClick();
+        }
+    }
+
+    private void LaunchBallOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX, velocityY);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddleToBallVector + paddlePos;
