00f08222232387889a8e42564a3a86072c11c3cb
unkeptroadrash <unkeptroadrash@outlook.com>
2021-02-11 21:39:48 -0800

    Ball now sticks to paddle on start.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    // configuration parameters
+    [SerializeField] Paddle paddle;
+
+    // state
+    Vector2 paddleToBallVector;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paddleToBallVector = transform.position - paddle.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddleToBallVector + paddlePos;
     }
 }
