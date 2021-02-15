2ec4024c5c5c5172f9e0542c5dca6b3f313b7c0a
unkeptroadrash <unkeptroadrash@outlook.com>
2021-02-15 00:06:40 -0800

    Score now carries with you on the next level.

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -14,6 +14,20 @@ public class GameStatus : MonoBehaviour
     // state variables
     [SerializeField] int currentScore = 0;
 
+    void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if(gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     void Start()
     {
         scoreText.text = currentScore.ToString();
