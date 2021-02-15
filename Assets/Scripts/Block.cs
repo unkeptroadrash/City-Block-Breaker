7e6f24597fe5b999f9bb37bb63b0cd8c1624852f
unkeptroadrash <unkeptroadrash@outlook.com>
2021-02-14 22:02:05 -0800

    Counting breakable blocks to use as win condition

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.countBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, new Vector3(0,0,0));
