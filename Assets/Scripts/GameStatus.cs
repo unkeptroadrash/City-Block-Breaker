c8714f20d5c76f70952c7302f4336597ea84753b
unkeptroadrash <unkeptroadrash@outlook.com>
2021-02-14 23:41:53 -0800

    Added Display Player Score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // configuration parameters
     [Range(0.1f,10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsAdded = 20;
+    [SerializeField] TextMeshProUGUI scoreText;
 
 
     // state variables
     [SerializeField] int currentScore = 0;
+
+    void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,5 +28,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsAdded;
+        scoreText.text = currentScore.ToString();
     }
 }
