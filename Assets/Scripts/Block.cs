01d1dbedb06eb984e30128e69674f9174dc6d609
unkeptroadrash <unkeptroadrash@outlook.com>
2021-02-14 21:48:22 -0800

    Added different sound effect when breaking blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] AudioClip breakSound;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, new Vector3(0,0,0));
         Destroy(gameObject);
     }
 }
